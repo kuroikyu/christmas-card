d30227ae0633d75bb3b46ce7921bde1ae8861c85
kuroikyu <kuroikyu@users.noreply.github.com>
2016-11-24 15:52:14 +0000

    24/11

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,36 @@
 (() => {
-  const card = document.getElementsByClassName('image')[0];
-
+  const card = document.getElementsByClassName('flip-container')[0]
   card.addEventListener('click', event => {
-    let cName = 'flipped';
-    let toggled = card.classList.contains(cName);
-    card.classList[toggled ? 'remove' : 'add'](cName);
+    let cName = 'hover';
+    let toggled = card.classList.contains(cName)
+    card.classList[toggled ? 'remove' : 'add'](cName)
   })
+})()
+
+function getQueryVariable(variable) {
+  let query = location.search.substring(1)
+  let vars = query.split('&')
+  for (let i=0; i<vars.length; i++) {
+    let pair = vars[i].split('=')
+    if (pair[0] == variable) {return pair[1]}
+  }
+    return false;
+}
+
+function getQueryValues() {
+  let query = location.search.substring(1)
+  let vars = query.split('&')
+  let values = []
+  for (let i=0; i<vars.length; i++) {
+    let out = vars[i].split('=')[1]
+    out = out.replace('%20', ' ')
+    values.push(out)
+  }
+    return values
+}
+
+
+(() => {
+  const targety = document.getElementsByClassName('names-target')[0]
+  targety.innerHTML = getQueryValues().join(', ')
 })()
