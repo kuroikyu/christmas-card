5dc0b2117de0f55df06d0cc916ee69d6b92393ec
kuroikyu <kuroikyu@users.noreply.github.com>
2016-12-21 10:26:55 +0000

    remove unnecessary commented section

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,16 +12,6 @@
   })
 })()
 
-// function getQueryVariable(variable) {
-//   let query = location.search.substring(1)
-//   let vars = query.split('&')
-//   for (let i=0; i<vars.length; i++) {
-//     let pair = vars[i].split('=')
-//     if (pair[0] == variable) {return pair[1]}
-//   }
-//     return false;
-// }
-
 function getQueryValues() {
   let query = location.search.substring(1)
   let vars = query.split('&')
